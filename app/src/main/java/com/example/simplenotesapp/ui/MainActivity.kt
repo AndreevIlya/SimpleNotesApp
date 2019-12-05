46b71886f8158c78227e6baf96cd4b6f2d7f64ec
AndreevIlya <katauri@yandex.ru>
2019-12-05 13:15:05 +0300

    Project structure imposed.

--- app/src/main/java/com/example/simplenotesapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/simplenotesapp/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.simplenotesapp
+package com.example.simplenotesapp.ui
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.simplenotesapp.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
